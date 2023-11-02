3ebfa00b4de42d62b91142404d5da93f588a1c66
agupta-04 <57514790+agupta-04@users.noreply.github.com>
2023-11-01 19:44:27 -0700

    Delete seenema-backend/main-screen.test.js

--- seenema-backend/main-screen.test.js
+++
@@ -1,8 +0,0 @@
-const loadMainScreen = () => {
-    // Stubbing the actual screen load process
-    return "Main screen loaded"; 
-};
-
-test('Main screen loads after login', () => {
-    expect(loadMainScreen()).toBe("Main screen loaded");
-});
