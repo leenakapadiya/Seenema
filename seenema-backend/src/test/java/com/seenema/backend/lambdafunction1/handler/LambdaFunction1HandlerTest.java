0e93285468e5b3a17143de53b88d27948ada4194
dishant <dgpagdar@cs.washington.edu>
2023-11-12 21:13:00 -0800

    Fixing CI Setup

---
+++ seenema-backend/src/test/java/com/seenema/backend/lambdafunction1/handler/LambdaFunction1HandlerTest.java
@@ -0,0 +1,11 @@
+package com.seenema.backend.lambdafunction1.handler;
+
+import org.testng.annotations.Test;
+
+public class LambdaFunction1HandlerTest {
+
+    @Test
+    public void testHandleRequest() {
+        // Add your test logic
+    }
+}
