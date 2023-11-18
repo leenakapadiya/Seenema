1599373a54fb804185f4fc259478b89f3dd8c690
dishant <dgpagdar@cs.washington.edu>
2023-11-17 17:53:33 -0800

    Added Sidebar in th Hompage with genres in it

--- seenema-frontend/src/Homepage/js/Homepage.js
+++ seenema-frontend/src/Homepage/js/Homepage.js
@@ -3,21 +3,32 @@ import Header from './Header';
 import MovieList from "./MovieList";
 import "../css/Homepage.css"
 import "../../App.css"
+import Sidebar from "./Sidebar";
 
 // Defining homepage component as a functional component
 function Homepage() {
     const [searchValue, setSearchValue] = useState('');
+    const [headerHeight, setHeaderHeight] = useState(0);
 
     const handleSearchChange = (value) => {
         setSearchValue(value);
     }
     return (
-        <div className="home">
-            {/* Rendering Header component */}
-            <Header onChange={handleSearchChange}/>
-              
-            {/* Rendering MovieList component */}
-            <MovieList searchValue={searchValue}/>
+        <div className="home-layout" style={{'--headerHeight': `${headerHeight}px`}}>
+            {/*Header Component */}
+            <div><Header onChange={handleSearchChange} onHeightChange={setHeaderHeight}/>
+            </div>
+            <div className="homepage-main-content">
+                {/* Sidebar */}
+                <div className="homepage-sidebar">
+                    <Sidebar/>
+                </div>
+                <div className="main-content-area">
+                    {/* Rendering MovieList component */}
+                    <MovieList searchValue={searchValue}/>
+                </div>
+
+            </div>
         </div>
     );
 }
