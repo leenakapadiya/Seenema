45ceba88102e182bbc9b1b4efcaf9e2f18eb7733
agupt <agupta04@uw.edu>
2023-11-29 12:24:34 -0800

    Added functionality for all browsers

--- seenema-frontend/src/Homepage/js/SearchBar.js
+++ seenema-frontend/src/Homepage/js/SearchBar.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Form, FormControl} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap';
+import {X} from 'react-bootstrap-icons'
 
 const SearchBar = ({onSearch}) => {
+    const [searchValue, setSearchValue] = useState("")
     const handleSearchEnter = (e) => {
         if (e.key === 'Enter') {
             if (e.target.value.trim() === "") {
@@ -10,16 +12,22 @@ const SearchBar = ({onSearch}) => {
             } else {
                 e.preventDefault();
                 const value = e.target.value.trim(); // Trim leading/trailing spaces
+                setSearchValue(value)
                 onSearch(value, true); // Trigger the search only when Enter is pressed
             }
         }
     };
 
     const handleSearchChange = (e) => {
+        setSearchValue(e.target.value.trim())
         if (e.target.value.trim() === "") {
             const value = "";
             onSearch(value);
         }
+    }
+
+    const clearSearch = () => {
+        onSearch("")
     }
 
     return (
@@ -34,6 +42,11 @@ const SearchBar = ({onSearch}) => {
                     onKeyDown={handleSearchEnter}
                     onChange={handleSearchChange}
                 />
+                 {searchValue && (
+                    <Button variant="light" onClick={clearSearch} style={{ border: 'none', backgroundColor: 'transparent' }}>
+                        <X size={20} />
+                    </Button>
+                )}
             </Form>
         </div>
     );
